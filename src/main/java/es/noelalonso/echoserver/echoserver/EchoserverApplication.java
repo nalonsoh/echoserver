6e685e55609f3a78f51e051b78342e9844c2ce5d
nalonsoh <nalonsoh@viewnext.com>
2019-02-19 09:12:46 +0100

    Commit inicial

---
+++ src/main/java/es/noelalonso/echoserver/echoserver/EchoserverApplication.java
@@ -0,0 +1,13 @@
+package es.noelalonso.echoserver.echoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoserverApplication.class, args);
+	}
+
+}
