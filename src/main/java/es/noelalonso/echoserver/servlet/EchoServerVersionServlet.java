9edb72306d4aca093bdc628c433f86e69f3a367d
nalonsoh <nalonsoh@viewnext.com>
2020-04-16 19:30:37 +0200

    added version servlet

---
+++ src/main/java/es/noelalonso/echoserver/servlet/EchoServerVersionServlet.java
@@ -0,0 +1,33 @@
+package es.noelalonso.echoserver.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@WebServlet("/version")
+public class EchoServerVersionServlet extends HttpServlet {
+
+	private static final String VERSION = "v1.1";
+
+	private static final Log LOGGER = LogFactory.getLog(EchoServerDefaultServlet.class);
+	
+	/** serialVersionUID */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LOGGER.info("EchoServerVersionServlet.service " + VERSION);
+		
+		response.getWriter().println(VERSION);
+		response.getWriter().flush();
+	}
+
+	
+}
